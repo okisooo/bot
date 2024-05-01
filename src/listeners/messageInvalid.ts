766b8799d7883c2359c9317c0840cc8317567cf2
oxo <samuelesantonicola@gmail.com>
2024-05-01 19:00:31 +0200

    Revert "returning error + else"
    This reverts commit 45157c61b460a08901af693be32ac3cbdcb70527.

--- src/listeners/messageInvalid.ts
+++ src/listeners/messageInvalid.ts
@@ -209,12 +209,9 @@ export default class MessageInvalid extends Listener {
           null,
           quote.channel != "debug.", // will return converter error msgs if using debug.discord.com
           quote
-        ).catch((err) => err); // return the caught error
-
-        if (convertedMessage instanceof Error) {
-          // handle the error here
-          return convertedMessage;
-        } else if (convertedMessage) {
+        ).catch(() => {});
+        // TODO: maybe return the caught error and add an else here?
+        if (convertedMessage) {
           const args = {
             quote: convertedMessage as FireMessage,
             debug: quote.channel == "debug.",
